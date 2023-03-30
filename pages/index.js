2a597d2a79026b26817fb5f7558e96bc7dba88a9
Thomas <Thomas.gertenbach99@gmail.com>
2023-03-31 00:34:36 +0200

    Auto stash before merge of "main" and "origin/main"

--- pages/index.js
+++ pages/index.js
@@ -44,53 +44,53 @@ export default function Index(props) {
 }
 
 export async function getStaticProps() {
-  const roomsResult = await fetch(
-    `${process.env.HUE_BRIDGE_IP}${process.env.HUE_BASE_URL}room`,
+  // const roomsResult = await fetch(
+  //   `${process.env.HUE_BRIDGE_IP}${process.env.HUE_BASE_URL}room`,
+  //   {
+  //     headers: {
+  //       "hue-application-key": process.env.HUE_APP_KEY,
+  //     },
+  //   }
+  // );
+  // const rooms = (await roomsResult.json()).data;
+
+  // const lightsResult = await fetch(
+  //   `${process.env.HUE_BRIDGE_IP}${process.env.HUE_BASE_URL}light`,
+  //   {
+  //     headers: {
+  //       "hue-application-key": process.env.HUE_APP_KEY,
+  //     },
+  //   }
+  // );
+  // const lights = (await lightsResult.json()).data;
+
+
+  const lights = [
     {
-      headers: {
-        "hue-application-key": process.env.HUE_APP_KEY,
-      },
-    }
-  );
-  const rooms = (await roomsResult.json()).data;
-
-  const lightsResult = await fetch(
-    `${process.env.HUE_BRIDGE_IP}${process.env.HUE_BASE_URL}light`,
+      id: "bd062eb7-b665-409d-a4d8-f7f37853d5d8",
+      color: { xy: { x: 0.5, y: 0.5 } },
+      dimming: { brightness: 100 },
+      on: { on: true },
+    },
     {
-      headers: {
-        "hue-application-key": process.env.HUE_APP_KEY,
-      },
-    }
-  );
-
-  const lights = (await lightsResult.json()).data;
-
-  // const lights = [
-  //   {
-  //     id: "bd062eb7-b665-409d-a4d8-f7f37853d5d8",
-  //     color: { xy: { x: 0.5, y: 0.5 } },
-  //     dimming: { brightness: 100 },
-  //     on: { on: true },
-  //   },
-  //   {
-  //     id: "933b40f7-8ca7-4e5e-9fbf-5994a6f641b1",
-  //     color: { xy: { x: 0.25, y: 0.75 } },
-  //     dimming: { brightness: 50 },
-  //     on: { on: false },
-  //   },
-  //   {
-  //     id: "8a51ba14-0c92-4068-9504-ee8280cb83fe",
-  //     color: { xy: { x: 0.75, y: 0.25 } },
-  //     dimming: { brightness: 75 },
-  //     on: { on: true },
-  //   },
-  //   {
-  //     id: "703a01e7-00c0-43c6-b1b9-a3884db0fcaa",
-  //     color: { xy: { x: 0.75, y: 0.25 } },
-  //     dimming: { brightness: 75 },
-  //     on: { on: true },
-  //   },
-  // ];
+      id: "933b40f7-8ca7-4e5e-9fbf-5994a6f641b1",
+      color: { xy: { x: 0.25, y: 0.75 } },
+      dimming: { brightness: 50 },
+      on: { on: false },
+    },
+    {
+      id: "8a51ba14-0c92-4068-9504-ee8280cb83fe",
+      color: { xy: { x: 0.75, y: 0.25 } },
+      dimming: { brightness: 75 },
+      on: { on: true },
+    },
+    {
+      id: "703a01e7-00c0-43c6-b1b9-a3884db0fcaa",
+      color: { xy: { x: 0.75, y: 0.25 } },
+      dimming: { brightness: 75 },
+      on: { on: true },
+    },
+  ];
 
   return {
     props: {
